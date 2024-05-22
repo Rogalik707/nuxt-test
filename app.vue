b5a45f5a5cd456e4cc69632830e23a76edb72d32
Maxim <etirel99@gmail.com>
2024-05-22 15:30:33 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>
+      <p style="margin-left: 20px; display: list-item; list-style-type: decimal"><span>test</span></p>
+      <p style="margin-left: 20px; display: list-item; list-style-type: decimal"><span>test</span></p>
+      <p style="margin-left: 20px; display: list-item; list-style-type: decimal"><span>test</span></p>
+    </div>
+
+    <div>
+      <p style="margin-left: 20px; display: list-item; list-style-type: decimal"><span>test</span></p>
+      <p style="margin-left: 20px; display: list-item; list-style-type: decimal"><span>test</span></p>
+      <p style="margin-left: 20px; display: list-item; list-style-type: decimal"><span>test</span></p>
+    </div>
+  </div>
+</template>
